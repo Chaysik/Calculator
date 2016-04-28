e9939c3c01f77f7784239ad6cc6529faa4258f4e
Chaysik <michpio97@gmail.com>
2016-04-28 20:12:37 +0200

    Added displays function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 window.onload = initAll;
+var input = '';
+var result = false;
+var display_handle = document.getElementById('display');
+
 
 function initAll() {
-	console.log('works');
+	var x = document.getElementsByClassName('button');
+	for (var i = 0;i< x.length;i++) {
+		x[i].onclick = function() {
+			input += this.value;
+			display_handle.innerHTML = input;
+		}
+	}
+
+	document.getElementById('clear').onclick = clear_input;
 }
+
+function clear_input() {
+	input='';
+	display_handle.innerHTML = input;
+}
+
